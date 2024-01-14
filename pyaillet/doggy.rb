35a62243c32a2fac61684d5d41a61efc18fc2fbf
pyaillet <11957179+pyaillet@users.noreply.github.com>
2024-01-14 08:13:17 +0000

    "Brew formula update for doggy version v0.4.2"

--- pyaillet/doggy.rb
+++ pyaillet/doggy.rb
@@ -5,31 +5,31 @@
 class Doggy < Formula
   desc "Minimal tui (terminal user interface) for docker"
   homepage "https://github.com/pyaillet/doggy"
-  url "https://github.com/pyaillet/doggy/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "e68a54ed888175ad3b81e98af76ef778624ec3edd194357bf9584adfcf7eb458"
+  url "https://github.com/pyaillet/doggy/archive/refs/tags/v0.4.2.tar.gz"
+  sha256 "1cd057bd3c9a3dc001cc0c668b66c1542e25dfc11ed95beb8afe76e417d75cdc"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/pyaillet/doggy/releases/download/v0.4.1/doggy-0.4.1-darwin-amd64.tar.gz"
-      sha256 "35214bdc10f10f2e15e915c5d9a4b266941af2f2ca02b479a04cfd691dcf4865"
+      url "https://github.com/pyaillet/doggy/releases/download/v0.4.2/doggy-0.4.2-darwin-amd64.tar.gz"
+      sha256 "5fc7b3bb5f18984ba6962f707dad7e1e12eee00581eb91656eb92d9ae4ebab51"
     end
 
     on_arm do
-      url "https://github.com/pyaillet/doggy/releases/download/v0.4.1/doggy-0.4.1-darwin-arm64.tar.gz"
-      sha256 "3e132744f2ea94de6963c0493d562cf1418ba22e8890d195564ca7bb53b40be8"
+      url "https://github.com/pyaillet/doggy/releases/download/v0.4.2/doggy-0.4.2-darwin-arm64.tar.gz"
+      sha256 "2ce483bc4eb986d8e7cd19da96b782cc3444583fa3a3f10933452f2085329380"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/pyaillet/doggy/releases/download/v0.4.1/doggy-0.4.1-linux-amd64.tar.gz"
-      sha256 "8347b89b29b289f191839e4e27c4c69160ce251a671301ae553dcfc5a80d9531"
+      url "https://github.com/pyaillet/doggy/releases/download/v0.4.2/doggy-0.4.2-linux-amd64.tar.gz"
+      sha256 "c2f22f9d5b3aebfcbf6c9fe19a220cce17033ea12bd785ef73d81c1a7ec96d31"
     end
 
     on_arm do
-      url "https://github.com/pyaillet/doggy/releases/download/v0.4.1/doggy-0.4.1-linux-arm64.tar.gz"
-      sha256 "5c63e8c9a296b5943f89d626c87ffa0323d2016ff62d7bddf895790e39741b58"
+      url "https://github.com/pyaillet/doggy/releases/download/v0.4.2/doggy-0.4.2-linux-arm64.tar.gz"
+      sha256 "47eccf13a559742009f1479e9c9d439677764b26c38d6a42a95a0ef8502d865c"
     end
   end
 
